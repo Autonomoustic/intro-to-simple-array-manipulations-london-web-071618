8983e68278771f9a9465b3ace023beedc43a8984
ali aziz <aa_connect@outlook.com>
2018-07-14 19:27:53 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,34 +1,34 @@
 def using_push(array, element)
   countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
   next_country = "Niger"
-  updated_array = countries_in_western_africa.push(next_country)
+  countries_in_western_africa.push(next_country)
 end
 
 def using_unshift(array, element)
   neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
   @new_neighborhood = "Brooklyn Heights"
-  @updated_array = neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhood)
+  neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhood)
 end
 
 def using_pop(array)
   @great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-  @deleted_string = @great_hits_of_the_nineties.pop
+  @great_hits_of_the_nineties.pop
 end
 
 def pop_with_args(array)
   @chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
-  @chars_arya_killed = @chars_in_game_of_thrones.pop(2)
+  @chars_in_game_of_thrones.pop(2)
 end
 
 def using_shift(array)
   @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  @im_so_over_this_city = @my_favorite_cities.first
+  @my_favorite_cities.first
   @my_favorite_cities.shift
 end
 
 def shift_with_args(array)
   @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  @brands_removed = @ice_cream_brands.shift(2)
+  @ice_cream_brands.shift(2)
 end
 
 def using_concat(array, element)
