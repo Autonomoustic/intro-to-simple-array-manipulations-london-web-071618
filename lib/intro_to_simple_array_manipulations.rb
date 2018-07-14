d2bbc15d68820de5749e1097fa3fad73b2480eb8
ali aziz <aa_connect@outlook.com>
2018-07-14 17:57:16 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,11 @@
-def using_push
+def using_push(array, element)
   countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
   next_country = "Niger"
   updated_array = countries_in_western_africa.push(next_country)
 end
 
-#def using_unshift
+def using_unshift(array, element)
+  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+  @new_neighborhood = "Brooklyn Heights"
+  @updated_array = neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhood)
+
