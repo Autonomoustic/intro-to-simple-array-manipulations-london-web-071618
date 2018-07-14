f7c2285f45b3ed59956118e85cc47b3472118b7f
ali aziz <aa_connect@outlook.com>
2018-07-14 18:45:51 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -46,4 +46,3 @@ end
 def using_uniq(array)
   captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
   new_array = captain_planet_and_the_planeteers.uniq
-  
