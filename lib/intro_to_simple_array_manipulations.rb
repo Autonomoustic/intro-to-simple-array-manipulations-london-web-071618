2cc2e8dc00223db086a26894b48c4e01a37cac23
ali aziz <aa_connect@outlook.com>
2018-07-14 18:35:30 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -41,4 +41,4 @@ def using_insert(array, element)
   @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
   @another_esoteric_language = "Malbolge"
   @new_array = @list_of_esoteric_programming_languages.insert(@another_esoteric_language)
-
+end
