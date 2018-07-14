7383e5f64b26db76b3d28855d07098f45057ee07
ali aziz <aa_connect@outlook.com>
2018-07-14 19:44:39 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,27 +19,23 @@ def using_shift(array)
 end
 
 def shift_with_args(array)
- array.shift(4)
+ array.shift
 end
 
 def using_concat(array1, array 2)
   array1.concat(array2)
 end
 
-def using_insert(array, element)
-  @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
-  @another_esoteric_language = "Malbolge"
-  @list_of_esoteric_programming_languages.insert(4, "Malbolge")
+def using_insert(array, item)
+ array.insert(4, item)
 end
 
 def using_uniq(array)
-  captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
-  captain_planet_and_the_planeteers.uniq
+ array.using
 end
 
 def using_flatten(array)
-  private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
-  private_colleges_in_newyork.flatten
+array.flatten
 end
 
 def using_delete(array, string)
