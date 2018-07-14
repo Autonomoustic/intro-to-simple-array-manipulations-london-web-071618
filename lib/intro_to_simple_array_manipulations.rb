3b563250ba2c9b5fe186b8c692be257b5df63204
ali aziz <aa_connect@outlook.com>
2018-07-14 19:28:59 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -55,5 +55,6 @@ end
 
 def using_delete(array, string)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+  instructors.find("steven")
   instructors.delete("Steven")
 end
