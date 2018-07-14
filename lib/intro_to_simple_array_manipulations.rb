f494e10d46e561cc4135437a53d568cc2e10084a
ali aziz <aa_connect@outlook.com>
2018-07-14 17:57:30 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,3 +8,4 @@ def using_unshift(array, element)
   neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
   @new_neighborhood = "Brooklyn Heights"
   @updated_array = neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhood)
+end
