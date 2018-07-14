7cc79e520e3a90a2f53100f8cd3dcb2a7d1afec5
ali aziz <aa_connect@outlook.com>
2018-07-14 19:25:08 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -54,5 +54,6 @@ def using_flatten(array)
 end
 
 def using_delete(array, string)
-  instructors.delete("steven")
+  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+  no_offense_steven = instructors.delete("Steven")
 end
